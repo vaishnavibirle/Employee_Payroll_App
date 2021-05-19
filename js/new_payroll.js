1e8c1a20a495fa070def1864561a0a382a060250
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-05-19 11:27:16 +0530

    [vaishnavi]ADDED: Event listener to display appropriate value on salary range

---
+++ js/new_payroll.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    /*event listener for name
+    * where it will query the name and checks valid or not
+    */
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+    /*event listener for salary
+    * where it will display the salary
+    */
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+
+});
