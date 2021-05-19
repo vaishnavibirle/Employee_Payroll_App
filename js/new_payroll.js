12611a9500f001df786cc4b37d9b689194d16e90
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-05-19 13:33:46 +0530

    [vaishnavi]ADDED:on clicking reset, resetting the form

--- js/new_payroll.js
+++ js/new_payroll.js
@@ -99,3 +99,33 @@ function createAndUpdateStorage(employeePayrollData){
     alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
 }
+
+/** UC5 Ability to reset the form */
+const resetForm = () => {
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2020');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setTextValue = (id , value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id , value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
